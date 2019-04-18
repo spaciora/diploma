ce6b39f062f869ea3577f1e235ba99fffe3e6480
spaciora <asya_nikky@mail.ru>
2019-04-18 14:59:51 +0700

    made points better

--- public/data.js
+++ public/data.js
@@ -3,8 +3,8 @@ var views = [
       name: "Hall1", //вход
       texture: "img/hall1.jpg",
       cameraTarget: { "x": 0.1, "y": 0, "z": -0.2 },
-      upAngle: Math.PI * 0.66, //чем меньше, тем меньше угол (max)
-      downAngle: Math.PI * 0.34, //чем больше, тем меньше угол (min)
+      upAngle: Math.PI * 0.63, //чем меньше, тем меньше угол (max)
+      downAngle: Math.PI * 0.37, //чем больше, тем меньше угол (min)
       points:
          [
             {
@@ -12,7 +12,7 @@ var views = [
                texture: "img/icons/link.png",
                data: "HallReception",
                rotation: 0.9,
-               coords: { "x": -0.5, "y": -0.7, "z": 2 }
+               coords: { "x": -2.8, "y": -2.7, "z": 8.5 }
             },
 
             {
@@ -20,7 +20,7 @@ var views = [
                texture: "img/icons/info.png",
                data: "info/HallReceptionInfo.txt",
                rotation: 0.6,
-               coords: { "x": -2, "y": 0, "z": -1 }
+               coords: { "x": -7, "y": 0, "z": -1 }
             },
 
             {
@@ -28,7 +28,7 @@ var views = [
                texture: "img/icons/link.png",
                data: "Hall2",
                rotation: 0.08,
-               coords: { "x": 0, "y": -0.6, "z": -2.2 }
+               coords: { "x": -0.5, "y": -2.4, "z": -8.2 }
             }
          ]
    },
@@ -72,30 +72,55 @@ var views = [
                texture: "img/icons/link.png",
                data: "Hall1",
                rotation: 0.9,
-               coords: { "x": -0.9, "y": -0.5, "z": 2 }
-            }
+               coords: { "x": -3.5, "y": -2, "z": 7.5 }
+            },
+
+            {
+               type: "link",
+               texture: "img/icons/link.png",
+               data: "Hall4",
+               rotation: -0.5,
+               coords: { "x": 8.5, "y": -2.2, "z": -0.8 }
+            },
          ]
    },
 
    {
       name: "Hall2", //холлIQ
       texture: "img/hall2.jpg",
-      cameraTarget: { "x": 0.1, "y": 0, "z": -0.2 },
-      upAngle: Math.PI * 0.53,
-      downAngle: Math.PI * 0.47,
+      cameraTarget: { "x": -0.3, "y": 0, "z": -0.2 },
+      upAngle: Math.PI * 0.525,
+      downAngle: Math.PI * 0.49,
       points:
          [
             {
                type: "link",
                texture: "img/icons/link.png",
                data: "Hall1",
-               rotation: 0.3,
-               coords: { "x": -1.5, "y": -0.5, "z": -1.5}
+               rotation: -1.8,
+               coords: { "x": -6, "y": -2.2, "z": -5.5 }
+            },
+
+            {
+               type: "link",
+               texture: "img/icons/link.png",
+               data: "Hall3",
+               rotation: -0.28,
+               coords: { "x": 6.2, "y": -2.1, "z": -4 }
+            },
+
+            {
+               type: "link",
+               texture: "img/icons/link.png",
+               data: "IQ1",
+               rotation: 1.3,
+               coords: { "x": 7.3, "y": -2.2, "z": 5 }
             },
          ]
    },
 
-   { name: "Hall3", //холлЗДЛ
+   {
+      name: "Hall3", //холлЗДЛ
       texture: "img/hall3.jpg",
       cameraTarget: { "x": 0.1, "y": 0, "z": -0.2 },
       upAngle: Math.PI * 0.66,
@@ -109,7 +134,52 @@ var views = [
                rotation: 0.9,
                coords: { "x": -0.5, "y": -0.7, "z": 2 }
             },
-         ]}
+         ]
+   },
+
+   {
+      name: "Hall4", //холлЗХЛ
+      texture: "img/hall4.jpg",
+      cameraTarget: { "x": -0.7, "y": 0, "z": 0 },
+      upAngle: Math.PI * 0.53,
+      downAngle: Math.PI * 0.465,
+      points:
+         [
+            {
+               type: "link",
+               texture: "img/icons/link.png",
+               data: "HallReception",
+               rotation: 0.3,
+               coords: { "x": -6.8, "y": -2.5, "z": -5 }
+            },
+
+            {
+               type: "link",
+               texture: "img/icons/link.png",
+               data: "Child",
+               rotation: -0.8,
+               coords: { "x": 5, "y": -3.3, "z": 7 }
+            },
+         ]
+   },
+
+   {
+      name: "Child",
+      texture: "img/child.jpg",
+      cameraTarget: { "x": 1, "y": 0, "z": 0 },
+      upAngle: Math.PI * 0.57,
+      downAngle: Math.PI * 0.43,
+      points:
+         [
+            {
+               type: "link",
+               texture: "img/icons/link.png",
+               data: "Hall4",
+               rotation: -0.7,
+               coords: { "x": 6.4, "y": -2.5, "z": 4 }
+            },
+         ]
+   }
 ];
 
 var panelIcons = [
@@ -133,7 +203,7 @@ var panelIcons = [
       data: "someData",
       coords: { "x": -0.1, "y": 0, "z": 0 }
    },
-   
+
    {
       name: "Right",
       texture: "img/icons/right.png",
